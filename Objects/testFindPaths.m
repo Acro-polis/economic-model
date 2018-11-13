84cfe48c4fb4c8009b71e4e53e63b2be4c7897be
Jess Taylor <jess@bluerocket.us>
2018-11-12 20:15:26 -0800

    Refactored & expanded testFindPaths unit test

--- Objects/testFindPaths.m
+++ Objects/testFindPaths.m
@@ -13,67 +13,65 @@ time = 1;
 polis = Polis(AM);
 polis.createAgents(time);
 
-fprintf("\nTest 1 - Connections between agents 10 and 9\n");
+fprintf("\n Testing Agent.findAllNetworkPathsToAgent()\n\n");
+
+testNumber = 1;
 sourceAgentId = 10;
 targetAgentId = 9;
-paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(AM, targetAgentId);
-polis.agents(sourceAgentId).logPaths(paths);
-[numPaths, ~] = size(paths);
-assert(numPaths == 1,"Test 1 failed, wrong number of total paths found");
-fprintf("\nGood, 1 Path From Source Agent %d To Target Agent %d\n", sourceAgentId, targetAgentId);
+expectedPaths = 1;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 
-fprintf("\nTest 2 - Connections between agents 1 and 8\n");
+testNumber = 2;
 sourceAgentId = 1;
 targetAgentId = 8;
-paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(AM, targetAgentId);
-polis.agents(sourceAgentId).logPaths(paths);
-[numPaths, ~] = size(paths);
-assert(numPaths == 6,"Test 2 failed, wrong number of total paths\n");
-fprintf("\nGood, 6 Paths From Source Agent %d To Target Agent %d\n", sourceAgentId, targetAgentId);
+expectedPaths = 6;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 
-fprintf("\nTest 3 - Connections between agents 1 and 10\n");
+testNumber = 3;
 sourceAgentId = 1;
 targetAgentId = 10;
-paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(AM, targetAgentId);
-polis.agents(sourceAgentId).logPaths(paths);
-[numPaths, ~] = size(paths);
-assert(numPaths == 6,"Test 3 failed, wrong number of total paths\n");
-fprintf("\nGood, 6 Paths From Source Agent %d To Target Agent %d\n", sourceAgentId, targetAgentId);
+expectedPaths = 6;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 
-fprintf("\nTest 4 - Connections between agents 10 and 1\n");
+testNumber = 4;
 sourceAgentId = 10;
 targetAgentId = 1;
-paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(AM, targetAgentId);
-polis.agents(sourceAgentId).logPaths(paths);
-[numPaths, ~] = size(paths);
-assert(numPaths == 6,"Test 4 failed, wrong number of total paths\n");
-fprintf("\nGood, 6 Paths From Source Agent %d To Target Agent %d\n", sourceAgentId, targetAgentId);
+expectedPaths = 6;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 
-fprintf("\nTest 5 - Connections between agents 10 and 1\n");
+testNumber = 5;
 sourceAgentId = 1;
 targetAgentId = 7;
-paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(AM, targetAgentId);
-polis.agents(sourceAgentId).logPaths(paths);
-[numPaths, ~] = size(paths);
-assert(numPaths == 6,"Test 5 failed, wrong number of total paths\n");
-fprintf("\nGood, 6 Paths From Source Agent %d To Target Agent %d\n", sourceAgentId, targetAgentId);
+expectedPaths = 6;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 
-fprintf("\nTest 6 - Connections between agents 10 and 1\n");
+testNumber = 6;
 sourceAgentId = 1;
 targetAgentId = 4;
-paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(AM, targetAgentId);
-polis.agents(sourceAgentId).logPaths(paths);
-[numPaths, ~] = size(paths);
-assert(numPaths == 5,"Test 6 failed, wrong number of total paths\n");
-fprintf("\nGood, 5 Paths From Source Agent %d To Target Agent %d\n", sourceAgentId, targetAgentId);
+expectedPaths = 5;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 
-fprintf("\nTest 7 - Connections between agents 10 and 1\n");
+testNumber = 7;
 sourceAgentId = 5;
 targetAgentId = 1;
-paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(AM, targetAgentId);
-polis.agents(sourceAgentId).logPaths(paths);
-[numPaths, ~] = size(paths);
-assert(numPaths == 6,"Test 7 failed, wrong number of total paths\n");
-fprintf("\nGood, 6 Paths From Source Agent %d To Target Agent %d\n", sourceAgentId, targetAgentId);
+expectedPaths = 6;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 
+testNumber = 7;
+sourceAgentId = 5;
+targetAgentId = 2;
+expectedPaths = 10;
+runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths);
 fprintf("\nTests Completed Successfully\n");
+
+%
+% Test Function
+%
+function runPathTest(polis, sourceAgentId, targetAgentId, testNumber, expectedPaths)
+    fprintf("\nTest %d - Testing connections between agents %d and %d\n", testNumber, sourceAgentId, targetAgentId);
+    paths = polis.agents(sourceAgentId).findAllNetworkPathsToAgent(polis.AM, targetAgentId);
+    polis.agents(sourceAgentId).logPaths(paths);
+    [numPaths, ~] = size(paths);
+    assert(numPaths == expectedPaths,"Test %d failed, expectd %d paths, found %d", testNumber, expectedPaths, numPaths);
+    fprintf("\nGood, %d Path(s) From Source Agent %d To Target Agent %d\n", expectedPaths, sourceAgentId, targetAgentId);
+end
