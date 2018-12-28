a9be244f9c3e9f3d120ed86f53a99f6503b67d19
Jess Taylor <jess@bluerocket.us>
2018-12-28 10:21:18 -0800

    Running Simulations

--- run_commerce.m
+++ run_commerce.m
@@ -10,10 +10,10 @@ clear;
 
 numberRuns = 3;
 
-fileNames = ["" ; ""];
-fileNames(1) = "inputCommerce_1.txt";
-fileNames(2) = "inputCommerce_2.txt";
-fileNames(3) = "inputCommerce_3.txt";
+fileNames = ["" ; "" ; ""];
+fileNames(1) = "InputCommerce_1.txt";
+fileNames(2) = "InputCommerce_2.txt";
+fileNames(3) = "InputCommerce_3.txt";
 
 %TODO - implement parfor
 for i = 1:numberRuns
