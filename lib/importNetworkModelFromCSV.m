1c94763920c5691a55a006fd9cfe98db9b6c2c4a
Jess Taylor <jess@Jesss-MBP.attlocal.net>
2019-09-17 12:53:26 -0700

    advancing simulations

--- lib/importNetworkModelFromCSV.m
+++ lib/importNetworkModelFromCSV.m
@@ -13,7 +13,12 @@ function AM = importNetworkModelFromCSV(N, fileName)
 AM = zeros(N,N);
 
 % Read the csv file that contains the network connections into a table
+
+% Use this for output from hybrid model (bring this one up-to-date)
 T = readtable(fileName,'Delimiter',',','Format','%d%d%s%s'); % Random Generator Output
+
+% Use this for current PA network generation model
+%T = readtable(fileName,'Delimiter',',','Format','%d%d%d%s%d%d%s'); % Random Generator Output
 
 % Build AM from the connections provided
 [connections, ~] = size(T);
